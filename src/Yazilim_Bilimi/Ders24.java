45f9dac595ba6d329eec5661e8c67fecef9f2f58
AkifAziz1 <aki.azizov.2014@gmail.com>
2022-03-30 23:21:49 +0400

    new Ders24

--- src/Yazilim_Bilimi/Ders24.java
+++ src/Yazilim_Bilimi/Ders24.java
@@ -1,4 +1,22 @@
 package Yazilim_Bilimi;
 
 public class Ders24 {
+    public static void main(String[] args) {
+
+        int [][] arr = {{1,2,3,} , {4,5,6}};
+
+        int [][] arr2 = new int[2][3];
+        arr2 [0][0] = 10;
+        arr2 [0][1] = 20;
+        arr2 [0][2] = 30;
+        arr2 [1][0] = 40;
+        arr2 [1][1] = 50;
+        arr2 [1][2] = 60;
+
+        for(int i = 0; i<2;i++){
+            for(int j = 0; j<3; j++){
+                System.out.println("Element: " + arr2[i][j]);
+            }
+        }
+    }
 }
