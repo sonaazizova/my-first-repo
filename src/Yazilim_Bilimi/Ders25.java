7cff2c1aa6034db4aa011184c00369f5f246d4c6
AkifAziz1 <aki.azizov.2014@gmail.com>
2022-03-31 23:12:57 +0400

    test contribitiobns

--- src/Yazilim_Bilimi/Ders25.java
+++ src/Yazilim_Bilimi/Ders25.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Ders25 {
     public static void main(String[] args) {
-        ArrayList<String> groups = new ArrayList<String>();
+        ArrayList <String> groups = new ArrayList<String>();
 
         //'add' funksiyasi ile arrayliste elave edirik
         groups.add("Metallica");
@@ -13,13 +13,22 @@ public class Ders25 {
         groups.add("Black Sabbath");
         groups.add("Ramstein");
 
+        //'remove' funksiyasi Arraylistin icindeki istediyimiz elementi silmek ucundur..
+        groups.remove("Ramstein");
+
+        //'remove' funksiyasina Arrayylisin icideki elementden elave index'ini de yazmaq mumkundur..
+        groups.remove(0);
+
+        //'indexOf' funksiyasi icine yazdigimiz elementin necenci index'de oldugunu gosterir
+        System.out.println(groups.indexOf("Iron Maiden"));
+
      /*   //'get' funksiyasinin icine almaq istediyimiz index'i yaziriq
         System.out.println("Birinci elemet: " + groups.get(0));
         System.out.println("Ikinci elemet: " + groups.get(1));
         System.out.println("Ucuncu elemet: " + groups.get(2));
         System.out.println("Dorduncu elemet: " + groups.get(3)); */
 
-        //System.out.println("Arraylist'in uzunlugu: " + groups.size());
+        System.out.println("Arraylist'in uzunlugu: " + groups.size());
 
         for(int i = 0; i<groups.size(); i++){
             System.out.println("Element: " + groups.get(i));
