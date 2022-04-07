3aad19e4ad189090a2f8150ec4403fcb5f752500
AkifAziz1 <aki.azizov.2014@gmail.com>
2022-04-07 23:11:47 +0400

    ATM

---
+++ src/Yazilim_Bilimi/ATM_practice.java
@@ -0,0 +1,14 @@
+package Yazilim_Bilimi;
+
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
+public class ATM_practice {
+    public static void main(String[] args) {
+        int balance;
+        balance = 1000;
+        System.out.println("cixaraciginiz meblegi daxil edin: ");
+        Scanner daxil = new Scanner(System.in);
+        i
+    }
+}
