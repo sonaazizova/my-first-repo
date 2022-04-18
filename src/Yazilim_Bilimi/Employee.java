4a38dacbaa01cf612cee01a2de2fefc40dee8cbe
AkifAziz1 <aki.azizov.2014@gmail.com>
2022-04-18 18:21:56 +0400

    +

--- src/Yazilim_Bilimi/Employee.java
+++ src/Yazilim_Bilimi/Employee.java
@@ -5,19 +5,18 @@ public class Employee {  // Ishchi
     private String department;
     private int salary;
 
-    public Employee(String name, String department, int salary){
+    public Employee(String name, String department, int salary) {
         this.name = name;
         this.department = department;
         this.salary = salary;
 
     }
-public void showInfos(){
-    System.out.println("Bilgiler: ");
-    System.out.println("Adi: " + this.name);
-    System.out.println("Söbə: " + this.department);
-    System.out.println("maashi: " + this.salary);
-}
 
-
+    public void showInfos() {
+        System.out.println("Bilgiler: ");
+        System.out.println("Adi: " + this.name);
+        System.out.println("Söbə: " + this.department);
+        System.out.println("maashi: " + this.salary);
     }
+}
 
--- src/Yazilim_Bilimi/Manager_Employee.java
+++ src/Yazilim_Bilimi/Manager_Employee.java
@@ -3,6 +3,7 @@ package Yazilim_Bilimi;
 public class Manager_Employee extends Employee{
 
     public Manager_Employee(String name, String department, int salary) {
+
         super(name, department, salary);
     }
 }
