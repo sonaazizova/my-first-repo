deb269d9026771f591c3d62efd283b978f75a74e
AkifAziz1 <aki.azizov.2014@gmail.com>
2022-08-09 20:48:04 +0400

    +

---
+++ src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+import static test.Student.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("db adi: " + dbName);
+        System.out.println("adi: " + name);
+        System.out.println("parolu: " + password);
+
+    }
+}
+
---
+++ src/test/Student.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Student {
+
+    public static String dbName = "test";
+    public static final String name = "Akif";
+    public static final String password = "12345";
+
+}
