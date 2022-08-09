be515bb54ec9418eefe2aa017acc7102fc34732d
AkifAziz1 <aki.azizov.2014@gmail.com>
2022-08-09 23:49:38 +0400

    +

--- src/SOLID4/Qush.java
+++ src/SOLID4/Qush.java
@@ -1,4 +1,9 @@
 package SOLID4;
 
+import javax.swing.plaf.basic.BasicDesktopIconUI;
+
 public interface Qush {
+    public static void main(String[] args) {
+        System.out.println("gush");
+    }
 }
