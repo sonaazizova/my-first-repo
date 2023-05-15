fad3a72634da306128fb2a275fdc309d556946d5
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-05-16 02:29:42 +0400

    +

--- src/Practice/test1.java
+++ src/Practice/test1.java
@@ -12,8 +12,6 @@ public class test1 {
 //        }
 
 
-
-
 //        for(int i=1;i<=9;i++){
 //            for(int j=1;j<=i;j++){
 //                System.out.print("* ");
@@ -23,20 +21,26 @@ public class test1 {
 //    }
 
 
+//        for (int i = 1; i < 8; i++) {
+//             for (int j = 0; j < 7 - i; j++) {
+//                System.out.print(" ");
+//
+//             }
+//             for (int j = 0; j < i; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println("");
+//        }
+//    }
+//}
 
 
-
-        for (int i = 1; i < 8; i++) {
-             for (int j = 0; j < 7 - i; j++) {
-                System.out.print(" ");
-
-             }
-             for (int j = 0; j < i; j++) {
-                 System.out.print("*");
-             }
-             System.out.println("");
+        for (int i = 0; i < 6; i++) {
+            System.out.print("= ");
+           for (int j= 3;;j++){
+               System.out.print(" ");
         }
+
     }
 }
-
-
+}
